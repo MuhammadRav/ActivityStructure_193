11fe0560dcd22b4021c53c74c70b215051fa23f4
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-04-03 14:22:21 +0700

    a

--- Structure/Structure.cpp
+++ Structure/Structure.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	int umur;
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
